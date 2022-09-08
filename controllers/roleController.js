985bc8a63d8b67d16a7a6402e40626be10f2bbaa
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-09-08 15:45:39 +0600

    Role Controller

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -1,1 +1,19 @@
 const Role = require('../models/userRoleModels')
+
+exports.getSingleRole = async (req,res,next)=>{
+    try{
+        const role = await Role.findById({id:req.params.id});
+        res.status(200).json({
+            status:'SUCCESS',
+            data:{
+                role
+            }
+        })
+
+    }catch(e){
+        console.log(`I am from GetSingleRole and Error is : ${e}`)
+        next(e)
+      } 
+
+};
+
